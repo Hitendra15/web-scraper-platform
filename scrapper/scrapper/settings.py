51c84fe4de43f6fee8a7f10f484e5af31be916d9
Hitendra15 <hitendramarathe15@gmail.com>
2026-05-12 07:45:42 +0530

    clear extracted links and apply pagination

--- scrapper/scrapper/settings.py
+++ scrapper/scrapper/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'link.apps.LinkConfig',
+    'core.apps.CoreConfig',
 ]
 
 MIDDLEWARE = [
